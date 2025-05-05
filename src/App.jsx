220b18a590326b3be22a44531866203d23277f9f
Ayinde Abrams <xxxenouniverse@gmail.com>
2025-05-04 22:57:18 -0400

    Added Showcase Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from './sections/hero.jsx'
+import Showcase from './sections/Showcase.jsx'
 
 const App = () => {
     return (
         <>
             <Hero />
+            <Showcase />
         </>
     )
 }
