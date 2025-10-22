28ad58c409ebc4b5906c555a088abaef48f9b990
NkosiDaniel <xxxenouniverse@gmail.com>
2025-10-21 21:51:20 -0400

    Added tech icons and tech stack to website

---
+++ src/components/Models/TechLogos/TechIcon.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Environment, Float, useGLTF } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+
+const TechIcon = ({model}) => {
+    const scene = useGLTF(model.modelPath);
+
+
+  return (
+    <Canvas>
+        <ambientLight intensity={0.3}/>
+        <Environment preset="city" />
+
+        <Float speed={5.5} rotationIntensity={0.5} floatIntensity={0.9}>
+            <group scale={model.scale}>
+                <primitive object={scene.scene}/>
+            </group>
+        </Float>
+    </Canvas> 
+  )
+}
+
+export default TechIcon
--- src/sections/techStack.jsx
+++ src/sections/techStack.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import TitleHeader from '../components/TitleHeader'
+import { techStackIcons } from '../constants/index.js'
+import TechIcon from '../components/Models/TechLogos/TechIcon.jsx'
 
 const techStack = () => {
   return (
     <div id="skills" className="flex-center section-padding">
-        <div>
+        <div className="w-full h-full md:px-10 px-5">
             <TitleHeader
                 title="My Preferred Tech Stack"
                 sub="🤝 What I'm Proficient In"
             />
+          <div className="tech-grid">
+              {techStackIcons.map((icon) => (
+                 <div key={icon.name} className="card-border tech-card overflow-hidden group xl:rounded-full rounded-lg"> 
+                    <div className="tech-card-animated-bg" /> 
+                      <div className="tech-card-content"> 
+                          <div className="tech-icon-wrapper">
+                            <TechIcon model={icon}/>
+                          </div>
+                          <div className="padding-x w-full"> 
+                             <p>{icon.name}</p>
+                          </div>
+                      </div>
+                    </div>
+              ))}
+          </div>
         </div>
     </div>
   )
