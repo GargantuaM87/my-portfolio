34949324433ab62ce874fd5deab2e46da47dd495
Ayinde Abrams <xxxenouniverse@gmail.com>
2025-10-22 22:24:36 -0400

    Finalized Footer. Now personal customization comes next

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -5,15 +5,20 @@ const Footer = () => {
   return (
     <footer className="footer">
         <div className="footer-container">
-            <div className="flex flex-col justify-center">
+            <div className="flex flex-col justify-center md:items-start items-center">
                 <a href="/">Visit my Blog</a>
             </div>
             <div className="socials">
                 {socialImgs.map((img) => (
-                    <a>
+                    <a className="icon" target="_blank" href={img.url} key={img.url}>
                         <img src={img.imgPath} />
                     </a>
                 ))}
+            </div>
+            <div className="flex flex-col justify-center">
+                <p className="text-center md:text-end">
+                  © {new Date().getFullYear()} Ayinde | All Rights Reserved.
+                </p>
             </div>
         </div>
     </footer>
