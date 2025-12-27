c8839bc7817d4468c032dedf62ecf84ea0f3347d
NkosiDaniel <xxxenouniverse@gmail.com>
2025-12-27 15:43:44 -0500

    Updated the Showcase section a little to reflect my recent projects

--- src/sections/Showcase.jsx
+++ src/sections/Showcase.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react'
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { useGSAP } from '@gsap/react'
+import TitleHeader from '../components/TitleHeader';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -40,29 +41,29 @@ const Showcase = () => {
     return (
         <section id="work" ref={sectionRef} className="app-showcase">
             <div className="w-full">
-                <h1>Showcase</h1>
+               <TitleHeader title="Showcase" sub="My Projects"/>
                 <div className="showcaselayout">
                     <div className="first-project-wrapper" ref={project1Ref}>
                         <div className="image-wrapper">
-                            <img src="/images/deltaspace.png" alt="Deltaspace" />
+                            <img src="/images/openGLProj.png" alt="" />
                         </div>
                         <div className="text-content">
-                            <h2>On-Demand Rides Made Simple with a Powerful, User-Friendly App called Ryde</h2>
+                            <h2>Small OpenGL Light Simulation</h2>
                             <p className="text-white-50 md:text-xl">
-                                An app built with React Native, Expo, & TailwindCSS for a fast, user-friendly experience.
+                                This is a simulation of Phong lighting in OpenGL, utilizing components such as ambient, diffuse, and specular lighting. 
+                                This project is tiny. 
+                                It consists of some floating cubes, an interactive camera, an ImGui interface for manipulating data, two point lights, a directional light, and a spot light. 
+                                I plan to gradually fix some features and add more as I continue my journey into Computer Graphics.
                             </p>
                         </div>
                     </div>
                     <div className="project-list-wrapper overflow-hidden">
                         <div className="project" ref={project2Ref}>
-                            <div className="image-wrapper bg-[#ffefdb]">
-                                <img src="/images/project2.png" alt="Library Management Platform" />
+                                <img src="/images/sinfulWaves.png" alt="A Game Submission" />
                             </div>
                             <h2>
-                                Library Management Platform: A Comprehensive Solution for Efficient Library Operations
+                                Sinful Waves - A Collaborative Game Off 2025 Submission 
                             </h2>
-                        </div>
-
                         <div className="project" ref={project3Ref}>
                             <div className="image-wrapper bg-[#ffe7eb]">
                                 <img src="/images/project3.png" alt="YC Directory" />
